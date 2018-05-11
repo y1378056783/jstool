0eab92418cd3ebae98705b83522b09279965eb8c
yyp <2081911650@qq.com>
2018-05-11 17:48:38 +0800

    添加功能函数

--- js/function.js
+++ js/function.js
@@ -630,6 +630,52 @@ basic.getRanArrEle = function(arr, count){
     }
     return shuffled.slice(min);
 }
+/*生成随机字符串*/
+basic.makeSalt = function (len) {
+    var str = 'qwertyuiop[]asdfghjkl;zxcvbnm,./';
+    var salt = '';
+    for(var i=0; i<len; i++){
+        salt += str.charAt(this.rand(0, str.length));
+    }
+    return salt;
+}
+basic.rand = function (max,min){
+    return Math.random()*(max-min+1) + min | 0;
+}
+/*去除重复*/
+basic.deleteRepetion = function (arr){
+ var arrTable = {},arrData = [];
+    for (var i = 0; i < arr.length; i++) {
+        if( !arrTable[ arr[i] ]){
+            arrTable[ arr[i] ] = true;
+            arrData.push(arr[i])
+         }
+     }
+    return arrData;
+}
+/*快速排序*/
+basic.quickSort = function (arr){
+    //如果数组<=1,则直接返回
+    if(arr.length<=1){return arr;}
+    var pivotIndex=Math.floor(arr.length/2);
+    //找基准，并把基准从原数组删除
+    var pivot=arr.splice(pivotIndex,1)[0];
+    //定义左右数组
+    var left=[];
+    var right=[];
+
+    //比基准小的放在left，比基准大的放在right
+    for(var i=0;i<arr.length;i++){
+        if(arr[i]<=pivot){
+            left.push(arr[i]);
+        }
+        else{
+            right.push(arr[i]);
+        }
+    }
+    //递归
+    return this.quickSort(left).concat([pivot],this.quickSort(right));
+} 
 /*
 非空判断
 */    
